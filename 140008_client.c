7b2dd27bcefd00ab61975ea92cfdb46e8cb7f5d0
Michael Shires <mshires1@gmail.com>
2014-09-30 21:34:16 -0400

    fixed whitespace, added help and s/n and version info in server, and added looping function to client to wait till command close.

--- 140008_client.c
+++ 140008_client.c
@@ -17,64 +17,72 @@
  * error - wrapper for perror
  */
 void error(char *msg) {
-    perror(msg);
-    exit(0);
+	perror(msg);
+	exit(0);
 }
 
 int main(int argc, char **argv) {
-    int sockfd, portno, n;
-    struct sockaddr_in serveraddr;
-    struct hostent *server;
-    char *hostname;
-    char buf[BUFSIZE];
+	int sockfd, portno, n;
+	struct sockaddr_in serveraddr;
+	struct hostent *server;
+	char *hostname;
+	char buf[BUFSIZE];
+	int cont = 0;
 
-    /* check command line arguments */
-    if (argc != 3) {
-       fprintf(stderr,"usage: %s <hostname> <port>\n", argv[0]);
-       exit(0);
-    }
-    hostname = argv[1];
-    portno = atoi(argv[2]);
+	while(cont == 0)
+	{
+		/* check command line arguments */
+		if (argc != 3) {
+			fprintf(stderr,"usage: %s <hostname> <port>\n", argv[0]);
+			exit(0);
+		}
+		hostname = argv[1];
+		portno = atoi(argv[2]);
+	
+		/* socket: create the socket */
+		sockfd = socket(AF_INET, SOCK_STREAM, 0);
+		if (sockfd < 0) 
+			error("ERROR opening socket");
 
-    /* socket: create the socket */
-    sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sockfd < 0) 
-        error("ERROR opening socket");
-
-    /* gethostbyname: get the server's DNS entry */
-    server = gethostbyname(hostname);
-    if (server == NULL) {
-        fprintf(stderr,"ERROR, no such host as %s\n", hostname);
-        exit(0);
-    }
-
-    /* build the server's Internet address */
-    bzero((char *) &serveraddr, sizeof(serveraddr));
-    serveraddr.sin_family = AF_INET;
-    bcopy((char *)server->h_addr, 
-	  (char *)&serveraddr.sin_addr.s_addr, server->h_length);
-    serveraddr.sin_port = htons(portno);
-
-    /* connect: create a connection with the server */
-    if (connect(sockfd, &serveraddr, sizeof(serveraddr)) < 0) 
-      error("ERROR connecting");
-
-    /* get message line from the user */
-    printf("Please enter msg: ");
-    bzero(buf, BUFSIZE);
-    fgets(buf, BUFSIZE, stdin);
-
-    /* send the message line to the server */
-    n = write(sockfd, buf, strlen(buf));
-    if (n < 0) 
-      error("ERROR writing to socket");
-
-    /* print the server's reply */
-    bzero(buf, BUFSIZE);
-    n = read(sockfd, buf, BUFSIZE);
-    if (n < 0) 
-      error("ERROR reading from socket");
-    printf("Echo from server: %s", buf);
-    close(sockfd);
+		/* gethostbyname: get the server's DNS entry */
+		server = gethostbyname(hostname);
+		if (server == NULL) {
+			fprintf(stderr,"ERROR, no such host as %s\n", hostname);
+			exit(0);
+		}
+	
+		/* build the server's Internet address */
+		bzero((char *) &serveraddr, sizeof(serveraddr));
+		serveraddr.sin_family = AF_INET;
+		bcopy((char *)server->h_addr, 
+			(char *)&serveraddr.sin_addr.s_addr, server->h_length);
+		serveraddr.sin_port = htons(portno);
+	
+		/* connect: create a connection with the server */
+		if (connect(sockfd, &serveraddr, sizeof(serveraddr)) < 0) 
+			error("ERROR connecting");
+	
+		/* get message line from the user */
+		printf("Please enter msg: ");
+		bzero(buf, BUFSIZE);
+		fgets(buf, BUFSIZE, stdin);
+	
+		/* send the message line to the server */
+		n = write(sockfd, buf, strlen(buf));
+		if (n < 0) 
+			error("ERROR writing to socket");
+	
+		/* print the server's reply */
+		bzero(buf, BUFSIZE);
+		n = read(sockfd, buf, BUFSIZE);
+		if (n < 0) 
+			error("ERROR reading from socket");
+		printf("Echo from server: %s", buf);
+		if(strcmp(buf, "close") == 0)
+		{
+			cont = 1;
+		}
+	}
+	close(sockfd);
     return 0;
 }
--- 140008_server.c
+++ 140008_server.c
@@ -22,15 +22,15 @@
 
 /* Internet address */
 struct in_addr {
-  unsigned int s_addr; 
+	unsigned int s_addr; 
 };
 
 /* Internet style socket address */
 struct sockaddr_in  {
-  unsigned short int sin_family; /* Address family */
-  unsigned short int sin_port;   /* Port number */
-  struct in_addr sin_addr;	 /* IP address */
-  unsigned char sin_zero[...];   /* Pad to size of 'struct sockaddr' */
+	unsigned short int sin_family;	/* Address family */
+	unsigned short int sin_port;	/* Port number */
+	struct in_addr sin_addr;	/* IP address */
+	unsigned char sin_zero[...];	/* Pad to size of 'struct sockaddr' */
 };
 
 /*
@@ -39,11 +39,11 @@ struct sockaddr_in  {
 
 /* Domain name service (DNS) host entry */
 struct hostent {
-  char    *h_name;        /* official name of host */
-  char    **h_aliases;    /* alias list */
-  int     h_addrtype;     /* host address type */
-  int     h_length;       /* length of address */
-  char    **h_addr_list;  /* list of addresses */
+	char	*h_name;	/* official name of host */
+	char	**h_aliases;	/* alias list */
+	int	h_addrtype;	/* host address type */
+	int	h_length;	/* length of address */
+	char	**h_addr_list;	/* list of addresses */
 }
 #endif
 
@@ -51,161 +51,155 @@ struct hostent {
  * error - wrapper for perror
  */
 void error(char *msg) {
-  perror(msg);
-  exit(1);
+	perror(msg);
+	exit(1);
 }
 
 int main(int argc, char **argv) {
-  int parentfd; /* parent socket */
-  int childfd; /* child socket */
-  int portno; /* port to listen on */
-  int clientlen; /* byte size of client's address */
-  struct sockaddr_in serveraddr; /* server's addr */
-  struct sockaddr_in clientaddr; /* client addr */
-  struct hostent *hostp; /* client host info */
-  char buf[BUFSIZE]; /* message buffer */
-  char input[BUFSIZE]; /*modifiable copy of buf*/
-  char* param[3]; /*holds seperated values*/
-  char *hostaddrp; /* dotted decimal host addr string */
-  int optval; /* flag value for setsockopt */
-  int n; /* message byte size */
-  int command; /*command type for agv*/
-  int comaddr; /*addr for command to go*/
-  int comval; /*value of command*/
+	int parentfd; /* parent socket */
+	int childfd; /* child socket */
+	int portno; /* port to listen on */
+	int clientlen; /* byte size of client's address */
+	struct sockaddr_in serveraddr; /* server's addr */
+	struct sockaddr_in clientaddr; /* client addr */
+	struct hostent *hostp; /* client host info */
+	char buf[BUFSIZE]; /* message buffer */
+	char input[BUFSIZE]; /*modifiable copy of buf*/
+	char* param[3]; /*holds seperated values*/
+	char *hostaddrp; /* dotted decimal host addr string */
+	int optval; /* flag value for setsockopt */
+	int n; /* message byte size */
+	int command; /*command type for agv*/
+	int comaddr; /*addr for command to go*/
+	int comval; /*value of command*/
 
-  /* 
-   * check command line arguments 
-   */
-  if (argc != 2) {
-    fprintf(stderr, "usage: %s <port>\n", argv[0]);
-    exit(1);
-  }
-  portno = atoi(argv[1]);
+	/* 
+	 * check command line arguments 
+	 */
+	if (argc != 2) {
+		fprintf(stderr, "usage: %s <port>\n", argv[0]);
+		exit(1);
+	}
+	portno = atoi(argv[1]);
 
-  /* 
-   * socket: create the parent socket 
-   */
-  parentfd = socket(AF_INET, SOCK_STREAM, 0);
-  if (parentfd < 0) 
-    error("ERROR opening socket");
+	/* 
+	 * socket: create the parent socket 
+	 */
+	parentfd = socket(AF_INET, SOCK_STREAM, 0);
+	if (parentfd < 0) 
+		error("ERROR opening socket");
 
-  /* setsockopt: Handy debugging trick that lets 
-   * us rerun the server immediately after we kill it; 
-   * otherwise we have to wait about 20 secs. 
-   * Eliminates "ERROR on binding: Address already in use" error. 
-   */
-  optval = 1;
-  setsockopt(parentfd, SOL_SOCKET, SO_REUSEADDR, 
-	     (const void *)&optval , sizeof(int));
+	/* setsockopt: Handy debugging trick that lets 
+	 * us rerun the server immediately after we kill it; 
+	 * otherwise we have to wait about 20 secs. 
+	 * Eliminates "ERROR on binding: Address already in use" error. 
+	 */
+	optval = 1;
+	setsockopt(parentfd, SOL_SOCKET, SO_REUSEADDR, 
+			 (const void *)&optval , sizeof(int));
 
-  /*
-   * build the server's Internet address
-   */
-  bzero((char *) &serveraddr, sizeof(serveraddr));
+	/*
+	 * build the server's Internet address
+	 */
+	bzero((char *) &serveraddr, sizeof(serveraddr));
 
-  /* this is an Internet address */
-  serveraddr.sin_family = AF_INET;
+	/* this is an Internet address */
+	serveraddr.sin_family = AF_INET;
 
-  /* let the system figure out our IP address */
-  serveraddr.sin_addr.s_addr = htonl(INADDR_ANY);
+	/* let the system figure out our IP address */
+	serveraddr.sin_addr.s_addr = htonl(INADDR_ANY);
 
-  /* this is the port we will listen on */
-  serveraddr.sin_port = htons((unsigned short)portno);
+	/* this is the port we will listen on */
+	serveraddr.sin_port = htons((unsigned short)portno);
 
-  /* 
-   * bind: associate the parent socket with a port 
-   */
-  if (bind(parentfd, (struct sockaddr *) &serveraddr, 
-	   sizeof(serveraddr)) < 0) 
-    error("ERROR on binding");
+	/* 
+	 * bind: associate the parent socket with a port 
+	 */
+	if (bind(parentfd, (struct sockaddr *) &serveraddr, 
+		sizeof(serveraddr)) < 0) 
+		error("ERROR on binding");
 
-  /* 
-   * listen: make this socket ready to accept connection requests 
-   */
-  if (listen(parentfd, 5) < 0) /* allow 5 requests to queue up */ 
-    error("ERROR on listen");
+	/* 
+	 * listen: make this socket ready to accept connection requests 
+	 */
+	if (listen(parentfd, 5) < 0) /* allow 5 requests to queue up */ 
+		error("ERROR on listen");
 
-  /* 
-   * main loop: wait for a connection request, echo input line, 
-   * then close connection.
-   */
-  clientlen = sizeof(clientaddr);
-  while (1) {
+	/* 
+	 * main loop: wait for a connection request, echo input line, 
+	 * then close connection.
+	 */
+	clientlen = sizeof(clientaddr);
+	while (1) {
 
-    /* 
-     * accept: wait for a connection request 
-     */
-    childfd = accept(parentfd, (struct sockaddr *) &clientaddr, &clientlen);
-    if (childfd < 0) 
-      error("ERROR on accept");
+		/* 
+		 * accept: wait for a connection request 
+		 */
+		childfd = accept(parentfd, (struct sockaddr *) &clientaddr, &clientlen);
+		if (childfd < 0) 
+			error("ERROR on accept");
     
-    /* 
-     * gethostbyaddr: determine who sent the message 
-     */
-    hostp = gethostbyaddr((const char *)&clientaddr.sin_addr.s_addr, 
-			  sizeof(clientaddr.sin_addr.s_addr), AF_INET);
-    if (hostp == NULL)
-      error("ERROR on gethostbyaddr");
-    hostaddrp = inet_ntoa(clientaddr.sin_addr);
-    if (hostaddrp == NULL)
-      error("ERROR on inet_ntoa\n");
-    printf("server established connection with %s (%s)\n", 
-	   hostp->h_name, hostaddrp);
+		/* 
+		 * gethostbyaddr: determine who sent the message 
+		 */
+		hostp = gethostbyaddr((const char *)&clientaddr.sin_addr.s_addr, 
+				sizeof(clientaddr.sin_addr.s_addr), AF_INET);
+		if (hostp == NULL)
+			error("ERROR on gethostbyaddr");
+		hostaddrp = inet_ntoa(clientaddr.sin_addr);
+		if (hostaddrp == NULL)
+			error("ERROR on inet_ntoa\n");
+		printf("server established connection with %s (%s)\n", hostp->h_name, hostaddrp);
     
-    /* 
-     * read: read input string from the client
-     */
-    bzero(buf, BUFSIZE);
-    bzero(input, BUFSIZE);
-    n = read(childfd, buf, BUFSIZE);
-    if (n < 0) 
-      error("ERROR reading from socket");
-    printf("server received %d bytes: %s", n, buf);
-    //for the future, strcmp(string, string) is useful for comparison
-    //between two strings. will return 0 if equal.
-    //while strtoken(char *str, const char *delim) is probably the
-    //most useful for parsing a string
-    param[0] = strtok(buf, "-");
-    param[1] = strtok(NULL, "-");
-    param[2] = strtok(NULL, "\0");
-    printf("%s:%s:%s\n", param[0], param[1], param[2]);
-    command = atoi(param[0]);
-    if(command == 1)
-      printf("Hello!");
-    else
-      printf("goodbye :(");
-    printf("hi! %d", command);
-    comaddr = atoi(param[1]);
-    comval = atoi(param[2]);
-    switch (command) {
-    case 1:
-      printf("hello!");
-      break;
-    case 2:
-      printf("DigitalRead");
-      break;
-    case 3:
-      printf("DigitalWrite");
-      break;
-    case 4:
-      printf("AnalogRead");
-      break;
-    case 5:
-      printf("AnalogWrite");
-      break;
-    case 6:
-      printf("pwmWrite");
-      break;
-    default:
-      printf("please input in int-int-int format");
-    }
-    /* 
-     * write: echo the input string back to the client 
-     */
-    n = write(childfd, buf, strlen(buf));
-    if (n < 0) 
-      error("ERROR writing to socket");
+		/* 
+		 * read: read input string from the client
+		 */
+		bzero(buf, BUFSIZE);
+		bzero(input, BUFSIZE);
+		n = read(childfd, buf, BUFSIZE);
+		if (n < 0) 
+			error("ERROR reading from socket");
+		printf("server received %d bytes: %s", n, buf);
+		//for the future, strcmp(string, string) is useful for comparison
+		//between two strings. will return 0 if equal.
+		//while strtoken(char *str, const char *delim) is probably the
+		//most useful for parsing a string
+		param[0] = strtok(buf, "-");
+		param[1] = strtok(NULL, "-");
+		param[2] = strtok(NULL, "\0");
+		printf("%s:%s:%s", param[0], param[1], param[2]);
+		command = atoi(param[0]);
+		comaddr = atoi(param[1]);
+		comval = atoi(param[2]);
+		switch (command) {
+		case 1:
+			printf("Flexicart S/N: 00 \nVersion 0.0.0\n");
+			break;
+		case 2:
+			printf("DigitalRead\n");
+			break;
+		case 3:
+			printf("DigitalWrite\n");
+			break;
+		case 4:
+			printf("AnalogRead\n");
+			break;
+		case 5:
+			printf("AnalogWrite\n");
+			break;
+		case 6:
+			printf("pwmWrite\n");
+			break;
+		default:
+			printf("please input in int-int-int format\n");
+		}
+		/* 
+		 * write: echo the input string back to the client 
+		 */
+		n = write(childfd, buf, strlen(buf));
+		if (n < 0) 
+			error("ERROR writing to socket");
 
-    close(childfd);
-  }
+		close(childfd);
+	}	
 }
